156edbe4df99f6d0a29241b40d62c22d4be9a632
fgsssb <jean.fgas@gmail.com>
2026-05-09 16:56:54 -0300

    Delete api directory

--- api/me.js
+++
@@ -1,7 +0,0 @@
-import { requireUser } from "./_utils.js";
-
-export default async function handler(req, res) {
-  const user = await requireUser(req, res);
-  if (!user) return;
-  return res.status(200).json({ ok: true, user });
-}
